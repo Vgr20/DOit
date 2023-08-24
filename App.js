bc4a917340d40d79bf9859374548d66739ccd26b
Vishagar Arunan <vishagara.20@uom.lk>
2023-08-24 15:14:56 +0530

    Combined Go...Get...On...
    Combined all Codes and Pushed it in to the master branch

--- App.js
+++ App.js
@@ -43,6 +43,12 @@ const navigator = createStackNavigator(
     },
     NavigationBarScreen : {
       screen : NavigationBar
+    },
+    FocusMode : {
+      screen : FocusMode
+    },
+    CountdownPage : {
+      screen : CountdownPage
     }
   },
   {
